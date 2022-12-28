3bf8ba45cd1f694f5f53b89f3cfc90a3cb4c78ac
ronak-khandelwal <ronak.khandelwal@cawstudios.com>
2022-12-28 16:43:58 +0530

    Register models

--- coreapis/admin.py
+++ coreapis/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import (
+    comments_model,
+    likes_model,
+    posts_model,
+    follow_model
+)
 
 # Register your models here.
+admin.site.register(comments_model.Comments)
+admin.site.register(likes_model.Likes)
+admin.site.register(posts_model.Posts)
+admin.site.register(follow_model.Follow)
