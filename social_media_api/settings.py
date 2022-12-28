f3ec8bd712f274375b77e3615b63cba807dd7a64
ronak-khandelwal <ronak.khandelwal@cawstudios.com>
2022-12-28 14:35:36 +0530

    Configure custom user model settings

--- social_media_api/settings.py
+++ social_media_api/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'coreapis'
+    'coreapis',
+    'authenticate'
 ]
 
 MIDDLEWARE = [
@@ -127,3 +128,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'authenticate.User'
