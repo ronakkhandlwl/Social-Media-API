6bd59f484f63b8a3d27e4fcee07bef4f53eebb6a
ronak-khandelwal <ronak.khandelwal@cawstudios.com>
2023-01-01 13:05:21 +0530

    Add path for \docs

--- social_media_api/urls.py
+++ social_media_api/urls.py
@@ -18,8 +18,21 @@ from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView
 from coreapis import urls
 
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Social Media API",
+        default_version='v1'
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/authenticate/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/', include(urls))
+    path('api/', include(urls)),
+    path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui')
 ]
